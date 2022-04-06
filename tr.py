4bdb5327eee0f1290ba09ae37b2c0304316175a6
iorala <andreas.laroi@gmail.com>
2022-04-06 13:05:11 +0200

    Funktionen hinzufügen

--- tr.py
+++ tr.py
@@ -4,14 +4,13 @@
 import gpxpy
 import gpxpy.gpx
 import pandas as pd
-import datetime
+
 
 def read_gpx(filename):
-# Import data from a gpx-file
+    # Import data from a gpx-file
     gpx_file = open(filename, 'r')
     gpx = gpxpy.parse(gpx_file)
     return gpx
-
 
 
 def create_df(gpx):
@@ -20,18 +19,19 @@ def create_df(gpx):
     for track in gpx.tracks:
         for segment in track.segments:
             for no, point in enumerate(segment.points):
-                df_list.append((point.time, point.latitude, point.longitude, point.elevation, point.extensions[0][1].text, segment.get_speed(no) * 3.6))  # * 3.6 -> m/ in km/h
-    df = pd.DataFrame.from_records(df_list, columns=['time', 'latitude', 'longitude', 'elevation', 'heartrate', 'speed'])
+                df_list.append((point.time, point.latitude, point.longitude, point.elevation,
+                                point.extensions[0][1].text, segment.get_speed(no) * 3.6))  # * 3.6 -> m/ in km/h
+    df = pd.DataFrame.from_records(df_list,
+                                   columns=['time', 'latitude', 'longitude', 'elevation', 'heartrate', 'speed'])
     return df
-
 
 
 def get_metadata(gpx):
     # get some metadata directly from the gpx-object
-    metadata = {"start_time": gpx.get_time_bounds().start_time
-                "end_time": gpx.get_time_bounds().end_time
+    metadata = {"start_time": gpx.get_time_bounds().start_time,
+                "end_time": gpx.get_time_bounds().end_time,
                 "duration": gpx.get_duration(),
                 "distance": gpx.length_3d(),
-                "climbs": get_uphill_downhill().uphill
+                "climbs": gpx.get_uphill_downhill().uphill
                 }
     return metadata
