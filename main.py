06ad8b56ce2624141aed3ca6b3ade59ceed87e5e
iorala <andreas.laroi@gmail.com>
2022-05-08 13:32:06 +0200

    Seitendiagramm anpassen

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-app = Flask("Formular")
+app = Flask("track-ride")
 
-@app.route('/hello')
+
+@app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 @app.route('/form', methods=["GET","POST"])
 def formular():
@@ -14,23 +16,48 @@ def formular():
         return "Formular empfangen"
     return render_template("formular.html")
 
-# # Benötigte Seiten
+
+@app.route('/', methods=["GET","POST"])
 # - Main page: main_page
-#     - Index.html
+def main_page():
+    # if request.method == "POST":
+    #    return "Formular empfangen"
+    #
+    return render_template("main.html")
+
+# - Streckenübersicht
+def show_routes():
+    # if request.method == "POST":
+    #     return "Formular empfangen"
+    return render_template("show_routes.html")
+
+def new_route():
 # - Strecke erstellen: new_route
-#     - Datei hochladen *
-#     - Medataten erfassen
-#     -> Streckenübersicht
+#     - Statische Seite mit Formular
+#     - Metadaten für Route
+#         - Name
+#         - Funktion (training, time trial, race)
+#     - Erste Fahrt erstellen - - Formular create_ride einbetten
+#           - GPX-Datei hochladen *
+#           - Medataten erfassen
+#     -> Formularinhalt mit POST an
+    return render_template("new_route.html")
+
+def new_ride():
+# - Neue Fahrt: new_ride
+#     - Formular ausfüllen
+#     - Datei hochladen
+#     - Metadaten erfassen
+#         - Namen, Evtl. dropdown mit training/pendeln
+#     -> Fahrt ansehen
+    return render_template("new_ride.html")
+
 # - Streckenübersicht: view_route
 #     - Auf Karte Zeichnen
 #     - Übersicht der gefahrenen Strecken
 #     - Allgemeine Statistiken (on the fly berechnet aus den Fahrten)
 #     - Button für neue Fahrt
-# - Neue Fahrt: new_ride
-#     - Datei hochladen *
-#     - Metadaten erfassen
-#         - Namen, Evtl. dropdown mit training/pendeln
-#     -> Fahrt ansehen
+
 # - Fahrten ansehen: view_ride
 #     - Einzelansicht der Fahrt
 #     - Auf Karte Zeichnen
@@ -44,6 +71,6 @@ def formular():
 #     - letzte fahrten mit Statistiken
 
 
-
+# App Ausführen
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
