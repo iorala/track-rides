5a3cbe682649729302e3561e937d46f45fb63fc8
iorala <andreas.laroi@gmail.com>
2022-07-03 19:00:00 +0200

    cleanup

--- tr.py
+++ tr.py
@@ -78,20 +78,3 @@ def create_dir(path):
 def read_track(filename):
     track = gpxo.Track(filename)
     return track
-
-# To do
-# - Draw map with Folium
-# - import folium
-#
-# m = folium.Map(location=[40.720, -73.993],
-#               zoom_start=15)
-#
-# loc = [(40.720, -73.993),
-#        (40.721, -73.996)]
-#
-# folium.PolyLine(loc,
-#                 color='red',
-#                 weight=15,
-#                 opacity=0.8).add_to(m)
-# m
-
